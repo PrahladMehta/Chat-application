6314525bc7e10a4f0d301eca76c6ed6f1ca364e4
prahladsinghmehta <prahladsinghmehta22@gmail.com>
2026-02-07 10:47:11 +0530

    Add: Vite config

--- src/pages/Setavatar.js
+++ src/pages/Setavatar.js
@@ -1,163 +1,173 @@
-import React from 'react'
-import { useState,useEffect} from 'react'
-import { useNavigate } from 'react-router-dom'
-import loader from "../assets/loader.gif"
-import { ToastContainer,toast } from 'react-toastify'
-import 'react-toastify/dist/ReactToastify.css';
+import React from "react";
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import loader from "../assets/loader.gif";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import axios from "axios";
-import { setAvatarRoute } from '../utils/Apiroutes';
-import styled  from "styled-components";
-import { Buffer } from 'buffer'
+import { setAvatarRoute } from "../utils/Apiroutes";
+import styled from "styled-components";
+import { Buffer } from "buffer";
+import multiavatar from "@multiavatar/multiavatar/esm";
+
 const SetAvatar = () => {
-  const api="https://api.multiavatar.com/45678945";
- const nav=useNavigate();
+  const nav = useNavigate();
 
- const [avatars,setAvatars]=useState([]);
- const[isLoading,setIsLoading]=useState(true);
- const[selectedAvatar,setSelectedAvatar]=useState();
-const toastOptions={
-  theme:"dark",
-  draggable:true,
-};
+  const [avatars, setAvatars] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [selectedAvatar, setSelectedAvatar] = useState();
+  const toastOptions = {
+    theme: "dark",
+    draggable: true,
+  };
 
-const setProfilePicture=async()=>{
+  const setProfilePicture = async () => {
+    if (selectedAvatar === undefined) {
+      toast.error("Please select an avatar", toastOptions);
+    } else {
+      const user = await JSON.parse(localStorage.getItem("chat-app-user"));
 
-  if(selectedAvatar===undefined){
-    toast.error("Please select an avatar",toastOptions);
-  }else{
-    const user=await JSON.parse(localStorage.getItem("chat-app-user"));
+      const { data } = await axios.post(`${setAvatarRoute}/${user._id}`, {
+        image: avatars[selectedAvatar],
+      });
 
-    const {data}=await axios.post(`${setAvatarRoute}/${user._id}`,{
-      image:avatars[selectedAvatar]
-    });
-  
-
-  if(data.isSet){
-    user.isAvatarImageSet=true;
-    user.avatarImage=data.image;
-    localStorage.setItem("chat-app-user",JSON.stringify(user));
-    nav("/");
-  }else{
-    toast.error("Error Setting avatar. Please try again",toastOptions);
-  }
-}
-
-}
-
-async function fetchData(){
-  try{ 
-    setIsLoading(true);
-     const data=[];
-    for(let i=0;i<4;i++){
-       const image=await axios.get(`${api}/${Math.round(Math.random()*1000)}`);
-       const buffer=new Buffer(image.data);
-       data.push(buffer.toString("base64"));
+      if (data.isSet) {
+        user.isAvatarImageSet = true;
+        user.avatarImage = data.image;
+        localStorage.setItem("chat-app-user", JSON.stringify(user));
+        nav("/");
+      } else {
+        toast.error("Error Setting avatar. Please try again", toastOptions);
+      }
     }
-    setAvatars(data);
-    setIsLoading(false);
-  }catch(e){
-    console.log(e);
-   
+  };
+
+  async function fetchData() {
+    try {
+      setIsLoading(true);
+      const data = [];
+
+      // Generate 4 unique avatars using Multiavatar
+      for (let i = 0; i < 4; i++) {
+        // Generate a random string for avatar ID
+        const randomId = `avatar_${Math.random().toString(36).substring(2, 15)}${Math.random().toString(36).substring(2, 15)}`;
+
+        // Generate SVG using Multiavatar
+        const svgCode = multiavatar(randomId);
+
+        // Convert SVG to base64
+        const buffer = new Buffer(svgCode);
+        data.push(buffer.toString("base64"));
+      }
+
+      setAvatars(data);
+      setIsLoading(false);
+    } catch (e) {
+      console.log("Error generating avatars:", e);
+      toast.error("Error generating avatars. Please try again.", toastOptions);
+      setIsLoading(false);
+    }
   }
-}
 
-
-useEffect(()=>{
-
-  if(!localStorage.getItem("chat-app-user")){
+  useEffect(() => {
+    if (!localStorage.getItem("chat-app-user")) {
       nav("/login");
-  }else{
-    fetchData();
-  }
-},[]);
+    } else {
+      fetchData();
+    }
+  }, []);
 
   return (
-   <>
-   {
-    isLoading?<Container>
-      <img src={loader} alt="loader" width="300" />
-    </Container>
-   :
-    <Container>
-      <div className="title-container">
-        <h1>Pick an avatar as your profile picture</h1>
+    <>
+      {isLoading ? (
+        <Container>
+          <img src={loader} alt="loader" width="300" />
+        </Container>
+      ) : (
+        <Container>
+          <div className="title-container">
+            <h1>Pick an avatar as your profile picture</h1>
+          </div>
+          <div className="avatars">
+            {avatars.map((avatar, idx) => {
+              return (
+                <div
+                  key={idx}
+                  className={`avatar ${selectedAvatar === idx ? "selected" : ""}`}
+                  onClick={() => setSelectedAvatar(idx)}
+                >
+                  <img
+                    src={`data:image/svg+xml;base64,${avatar}`}
+                    alt="avatar"
+                    width="100"
+                  />
+                </div>
+              );
+            })}
+          </div>
+          <button className="submit-btn" onClick={setProfilePicture}>
+            Set Profile Picture
+          </button>
+        </Container>
+      )}
+      <ToastContainer></ToastContainer>
+    </>
+  );
+};
 
-      </div>
-      <div className="avatars">
-        {
-          avatars.map((avatar,idx)=>{
-            return <div key={idx} className={`avatar ${selectedAvatar===idx?"selected":""}`} onClick={()=>setSelectedAvatar(idx)}>
-
-                <img src={`data:image/svg+xml;base64,${avatar}`} alt="avatar"  width="100"/>
-            </div>
-          })
-        }
-      </div>
-      <button className='submit-btn' onClick={setProfilePicture}>Set Profile Picture</button>
-    </Container>
-    }
-    <ToastContainer></ToastContainer>
-   </>
-  )
-}
-
-
-const Container=styled.div`
-display:flex;
-justify-content:center;
-align-items:center;
-flex-direction:column;
-gap:3rem;
-background-color:#131324;
-height:100vh;
-widht:100vw;
-.loader{
-  max-inline-size:100%;
-}
-
-.title-container{
-  h1{
-    color:white;
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  gap: 3rem;
+  background-color: #131324;
+  height: 100vh;
+  width: 100vw;
+  .loader {
+    max-inline-size: 100%;
   }
-}
-.avatars{
-  display:flex;
-  gap:2rem;
-  .avatar{
-    border:0.4rem solid transparent;
-    padding:0.4rem;
-    border-radius:5rem;
-    display:flex;
-    justify-content:center;
-    align-items:center;
-    transition:0.5s ease-in-out;
-    img{
-      height:6rem;
-    }
 
-  
+  .title-container {
+    h1 {
+      color: white;
+    }
   }
-  .selected{
-      border:0.4rem solid #4e0eff;
+  .avatars {
+    display: flex;
+    gap: 2rem;
+    .avatar {
+      border: 0.4rem solid transparent;
+      padding: 0.4rem;
+      border-radius: 5rem;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      transition: 0.5s ease-in-out;
+      cursor: pointer;
+      img {
+        height: 6rem;
+        border-radius: 50%;
+      }
     }
-}
-.submit-btn{
-   
-   background-color:#997af0;
-   color:white;
-   padding:1rem 2rem;
-   border:none;
-   cursor:pointer;
-   border-radius:0.4rem;
-   font-size:1rem;
-   text-transform:uppercase;
-   transition:.5s ease-in;
-   &:hover{
-     background-color:#4e0eff;
-   }
-
- }
+    .selected {
+      border: 0.4rem solid #4e0eff;
+    }
+  }
+  .submit-btn {
+    background-color: #997af0;
+    color: white;
+    padding: 1rem 2rem;
+    border: none;
+    cursor: pointer;
+    border-radius: 0.4rem;
+    font-size: 1rem;
+    text-transform: uppercase;
+    transition: 0.5s ease-in;
+    &:hover {
+      background-color: #4e0eff;
+    }
+  }
 `;
 
-
-export default SetAvatar
+export default SetAvatar;
--- src/utils/Apiroutes.js
+++ src/utils/Apiroutes.js
@@ -1,7 +1,7 @@
-export const  host="https://chat-application-backend-a7fr.onrender.com";
-export const registerRouter=`${host}/api/auth/register`;
-export const loginRouter=`${host}/api/auth/login`
-export const setAvatarRoute=`${host}/api/auth/setavatar`
-export const allUserRoute=`${host}/api/auth/allusers`
-export const sendMessageRoute=`${host}/api/auth/addmsg`
-export const getAllMessageRoute=`${host}/api/auth/getmsg`
+export const host = "http://localhost:3002";
+export const registerRouter = `${host}/api/auth/register`;
+export const loginRouter = `${host}/api/auth/login`;
+export const setAvatarRoute = `${host}/api/auth/setavatar`;
+export const allUserRoute = `${host}/api/auth/allusers`;
+export const sendMessageRoute = `${host}/api/auth/addmsg`;
+export const getAllMessageRoute = `${host}/api/auth/getmsg`;
